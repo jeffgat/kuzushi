63e1e978d80aecaf5e8d3a9e121c128b9e42f497
jeffgat <jeff.gtbn@gmail.com>
2023-06-12 14:59:01 -0600

    setup modal to add submissions

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,80 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Box,
     Center,
     Heading,
     VStack,
     Text,
-    Skeleton,
-    Checkbox,
-    HStack,
-    Icon,
-    IconButton,
     Input,
-    useToast,
+    Modal,
+    Button,
+    FormControl,
+    Icon,
+    Flex,
 } from 'native-base';
-import { Feather, Entypo } from '@expo/vector-icons';
+import { Feather, Entypo, Ionicons } from '@expo/vector-icons';
 
+// types
 type Props = {};
 
+const ModalItem = () => (
+    <Flex
+        direction="row"
+        align="center"
+        justify="space-between"
+        w="full"
+        h="20"
+        bg="blue.300"
+        rounded="md"
+        shadow={2}>
+        <Flex direction="row" align="center" justify="center" h="100%">
+            <Box w={16} h="100%" bg="rust.100" />
+
+            <Box ml={4}>
+                <Text color="neutral.50" fontSize={20} fontWeight={600}>
+                    Gullotine
+                </Text>
+                <Text color="neutral.50">Item description</Text>
+            </Box>
+        </Flex>
+        <Box pr={4}>
+            <Ionicons name="information-circle" size={24} color="white" />
+        </Box>
+    </Flex>
+);
+
+// constants
+const MODAL_BG = 'neutral.800';
+
 const Home = (props: Props) => {
-    const instState = [
-        {
-            title: 'Code',
-            isCompleted: true,
-        },
-        {
-            title: 'Meeting with team at 9',
-            isCompleted: false,
-        },
-        {
-            title: 'Check Emails',
-            isCompleted: false,
-        },
-        {
-            title: 'Write an article',
-            isCompleted: false,
-        },
-    ];
-    const [list, setList] = React.useState(instState);
-    const [inputValue, setInputValue] = React.useState('');
-    // const toast = useToast();
-
-    const addItem = (title: string) => {
-        // if (title === '') {
-        //     toast.show({
-        //         title: 'Please Enter Text',
-        //         status: 'warning',
-        //     });
-        //     return;
-        // }
-
-        setList((prevList) => {
-            return [
-                ...prevList,
-                {
-                    title: title,
-                    isCompleted: false,
-                },
-            ];
-        });
-    };
-
-    const handleDelete = (index) => {
-        setList((prevList) => {
-            const temp = prevList.filter((_, itemI) => itemI !== index);
-            return temp;
-        });
-    };
-
-    const handleStatusChange = (index) => {
-        setList((prevList) => {
-            const newList = [...prevList];
-            newList[index].isCompleted = !newList[index].isCompleted;
-            return newList;
-        });
-    };
-
+    const [showModal, setShowModal] = useState(false);
     return (
         <VStack bg="neutral.800" width="100%" height="100%">
             <Center>
@@ -82,83 +55,86 @@ const Home = (props: Props) => {
                     <Heading color="neutral.100">Kuzushi</Heading>
                     <Text color="neutral.300">Log your jits performance over time.</Text>
                 </VStack>
-                <Box maxW="300" w="100%" mt={8}>
-                    <Heading mb="2" size="md" color="neutral.50">
-                        Todos
-                    </Heading>
-                    <VStack space={4}>
-                        <HStack space={2}>
-                            <Input
-                                flex={1}
-                                onChangeText={(v) => setInputValue(v)}
-                                value={inputValue}
-                                placeholder="Add Task"
-                            />
-                            <IconButton
-                                borderRadius="sm"
-                                variant="solid"
-                                icon={
-                                    <Icon
-                                        as={Feather}
-                                        name="plus"
-                                        size="sm"
-                                        color="neutral.50"
-                                    />
-                                }
-                                onPress={() => {
-                                    addItem(inputValue);
-                                    setInputValue('');
-                                }}
-                            />
-                        </HStack>
-                        <VStack space={2}>
-                            {list.map((item, itemI) => (
-                                <HStack
-                                    w="100%"
-                                    justifyContent="space-between"
-                                    alignItems="center"
-                                    key={item.title + itemI.toString()}>
-                                    <Checkbox
-                                        isChecked={item.isCompleted}
-                                        onChange={() => handleStatusChange(itemI)}
-                                        value={item.title}></Checkbox>
-                                    <Text
-                                        width="100%"
-                                        flexShrink={1}
-                                        textAlign="left"
-                                        mx="2"
-                                        strikeThrough={item.isCompleted}
-                                        _light={{
-                                            color: item.isCompleted
-                                                ? 'neutral.400'
-                                                : 'neutral.800',
-                                        }}
-                                        _dark={{
-                                            color: item.isCompleted
-                                                ? 'neutral.400'
-                                                : 'neutral.50',
-                                        }}
-                                        onPress={() => handleStatusChange(itemI)}>
-                                        {item.title}
-                                    </Text>
-                                    <IconButton
-                                        size="sm"
-                                        colorScheme="neutral"
-                                        icon={
-                                            <Icon
-                                                as={Entypo}
-                                                name="minus"
-                                                size="xs"
-                                                color="neutral.400"
-                                            />
-                                        }
-                                        onPress={() => handleDelete(itemI)}
-                                    />
-                                </HStack>
-                            ))}
-                        </VStack>
-                    </VStack>
-                </Box>
+                <Center mt={4}>
+                    <Text color="neutral.300">Open modal to add submission</Text>
+                    <Button
+                        bg="rust.400"
+                        mt={2}
+                        _pressed={{
+                            bg: 'rust.600',
+                        }}
+                        onPress={() => setShowModal(true)}
+                        endIcon={
+                            <Icon as={Entypo} name="chevron-right" size="sm" ml={-1} />
+                        }>
+                        <Text fontWeight={600} color="white">
+                            Add
+                        </Text>
+                    </Button>
+                    <Modal
+                        isOpen={showModal}
+                        onClose={() => setShowModal(false)}
+                        animationPreset="slide"
+                        // _slide={{ duration: 400 }}
+                        size="full"
+                        borderColor="red">
+                        <Modal.Content
+                            borderColor="red"
+                            bg={MODAL_BG}
+                            marginBottom={0}
+                            marginTop="auto"
+                            h="full">
+                            <Modal.CloseButton />
+                            <Modal.Header bg={MODAL_BG} borderBottomColor="neutral.600">
+                                <Text color="neutral.50" fontSize="lg" fontWeight={700}>
+                                    Add submission
+                                </Text>
+                            </Modal.Header>
+                            <Modal.Body>
+                                {/* <FormControl>
+                                    <FormControl.Label>
+                                        <Text color="neutral.100">Name</Text>
+                                    </FormControl.Label>
+                                    <Input />
+                                </FormControl>
+                                <FormControl mt="3">
+                                    <Text color="neutral.100">Email</Text>
+                                    <Input />
+                                </FormControl> */}
+                                <VStack space={4} alignItems="center">
+                                    <ModalItem />
+                                    <ModalItem />
+                                    <ModalItem />
+                                </VStack>
+                                ;
+                            </Modal.Body>
+                            <Modal.Footer
+                                bg={MODAL_BG}
+                                borderTopColor="neutral.600"
+                                safeAreaBottom
+                                safeAreaX>
+                                <Button.Group space={2}>
+                                    <Button
+                                        variant="outline"
+                                        borderColor="rust.400"
+                                        onPress={() => {
+                                            setShowModal(false);
+                                        }}>
+                                        <Text color="rust.400">Cancel</Text>
+                                    </Button>
+                                    <Button
+                                        bg="rust.400"
+                                        onPress={() => {
+                                            setShowModal(false);
+                                        }}>
+                                        Save
+                                    </Button>
+                                </Button.Group>
+                            </Modal.Footer>
+                        </Modal.Content>
+                    </Modal>
+                </Center>
+                ;
             </Center>
         </VStack>
     );
